f82dd3997de038babbc58e2eb2bfa62e8a4e4219
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-10-29 15:17:15 +0300

    Small tweaks.

--- inc/functions-edd.php
+++ inc/functions-edd.php
@@ -4,7 +4,7 @@
  *
  * @package Checathlon
  */
- 
+
 // Disable plugin styles.
 add_filter( 'edd_get_option_disable_styles', '__return_true' );
 
@@ -17,21 +17,19 @@ add_filter( 'edd_get_option_disable_styles', '__return_true' );
  * @return string
  */
 function chucathlon_get_download_price() {
-	
 	// Set price.
 	$price = '';
-	
+
 	if ( edd_has_variable_prices( get_the_ID() ) ) :
-		$price = '<span class="screen-reader-text">' . esc_html__( 'Price:', 'checathlon' ) . ' </span>' . edd_price_range( get_the_ID() );			
-	elseif ( '0' != edd_get_download_price( get_the_ID() ) && ! edd_has_variable_prices( get_the_ID() ) ) :	
+		$price = '<span class="screen-reader-text">' . esc_html__( 'Price:', 'checathlon' ) . ' </span>' . edd_price_range( get_the_ID() );
+	elseif ( '0' != edd_get_download_price( get_the_ID() ) && ! edd_has_variable_prices( get_the_ID() ) ) :
 		$price = '<span class="screen-reader-text">' . esc_html__( 'Price:', 'checathlon' ) . ' </span>';
 		$price .= edd_price( get_the_ID(), false );
 	else :
 		$price = esc_html__( 'Free', 'checathlon' );
 	endif;
-	
+
 	return $price;
-	
 }
 
 /**
@@ -42,13 +40,13 @@ function chucathlon_get_download_price() {
  */
 function chucathlon_download_category_template( $template ) {
 
-	if ( is_tax( 'download_category' ) || is_tax( 'download_tag' ) ) {
-		
+	if ( is_tax( 'download_category' ) || is_tax( 'download_tag' ) || is_tax( 'edd_download_info_feature' ) ) {
+
 		$new_template = locate_template( array( 'archive-download.php' ) );
 		if ( '' != $new_template ) {
 			return $new_template ;
 		}
-		
+
 	}
 
 	return $template;
@@ -62,25 +60,25 @@ add_filter( 'taxonomy_template', 'chucathlon_download_category_template', 99 );
  * @return string
  */
 function chucathlon_download_get_demo_link_url() {
-	
+
 	// Set demo link url.
 	$demo_link_url = '';
-	
+
 	// Bail if EDD Download Info Plugin is not around.
 	if( ! function_exists( 'edd_download_info_get_download_demo_link' ) ) {
 		return $demo_link_url;
 	}
-	
+
 	// Get demo link url from EDD Download Info.
 	$demo_link_valid_url = edd_download_info_get_download_demo_link();
-	
+
 	// Return demo link url.
 	if( isset( $demo_link_valid_url ) && $demo_link_valid_url ) {
 		$demo_link_url = $demo_link_valid_url;
 	}
-	
+
 	return $demo_link_url;
-	
+
 }
 
 /**
@@ -90,16 +88,14 @@ function chucathlon_download_get_demo_link_url() {
  * @return string
  */
 function chucathlon_download_get_demo_link() {
-	
 	// Set demo link.
 	$demo_link = '';
-	
-	// Check demo if we have demo link.
+
+	// Check if we have demo link.
 	if( '' !== chucathlon_download_get_demo_link_url() ) {
 		$demo_link = '<a class="demo-link button button-secondary" href="' . esc_url( chucathlon_download_get_demo_link_url() ) . '">' . esc_html__( 'Demo', 'checathlon' ) . '</a>';
 	}
-	
+
 	// Return demo link.
 	return $demo_link;
-	
 }
