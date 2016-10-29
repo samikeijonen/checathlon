428a8beebd7303578933392775cc9065824b1a7e
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-10-29 15:19:41 +0300

    Move widget area template files to it's own folder.

--- components/content-service-pricing.php
+++ components/content-service-pricing.php
@@ -10,7 +10,7 @@ if ( ! checathlon_get_fp_hide_service_pricing() && checathlon_has_service_pricin
 	<div id="front-page-pricing-area" class="front-page-pricing-area front-page-area">
 		<?php
 			echo checathlon_get_pricing_area_title_html();
-			get_template_part( 'sidebar-pricing-page' );
+			get_template_part( 'widget-areas/sidebar', 'pricing-page' );
 		?>
 	</div><!-- .front-page-pricing-area -->
 <?php endif;
--- template-parts/content-download.php
+++ template-parts/content-download.php
@@ -12,19 +12,19 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<?php if ( is_singular() && ! checathlon_is_featured_page() && ! checathlon_is_front_page() ) : // If single. ?>
-	
+
 		<div class="entry-inner-singular">
-	
+
 			<header class="entry-header page-header text-center">
 				<?php the_title( '<h1 class="entry-title title-font no-margin-bottom text-italic">', '</h1>' ); ?>
 			</header><!-- .entry-header -->
-		
+
 			<?php checathlon_post_thumbnail( $post_thumbnail = 'checathlon-singular' ) ?>
-			
+
 			<div class="entry-inner-singular-wrapper">
-			
-				<?php get_sidebar( 'downloads' ); ?>
-			
+
+				<?php get_template_part( 'widget-areas/sidebar', 'downloads' ); // Loads the sidebar-downloads.php template.?>
+
 				<div class="entry-inner-content">
 					<div class="entry-content">
 						<?php
@@ -32,26 +32,26 @@
 						?>
 					</div><!-- .entry-content -->
 				</div><!-- .entry-inner-content -->
-				
+
 			</div><!-- .entry-inner-singular-wrapper -->
-			
+
 		</div><!-- .entry-inner-singular -->
-	
+
 	<?php else : ?>
-	
+
 		<div class="entry-inner-wrapper">
-	
+
 		<?php checathlon_post_thumbnail( $post_thumbnail = 'checathlon-product' ); ?>
-	
+
 		<div class="entry-inner">
-			
+
 			<header class="entry-header">
 				<?php
 					get_template_part( 'entry-meta' );
 					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 				?>
 			</header><!-- .entry-header -->
-		
+
 			<div class="entry-summary">
 				<?php
 					echo '<p class="product-price-p product-price soft-color medium-font-weight">' . chucathlon_get_download_price() . '</p>';
@@ -59,15 +59,15 @@
 					the_excerpt();
 				?>
 			</div><!-- .entry-summary -->
-			
+
 		</div><!-- .entry-inner -->
-		
+
 		<?php
 			get_template_part( 'entry-product-info' );
 		?>
-		
+
 	</div><!-- .entry-inner-wrapper -->
 
 	<?php endif; // End check single. ?>
-	
+
 </article><!-- #post-## -->
