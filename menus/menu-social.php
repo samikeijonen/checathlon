9a9a5dd7c051736b40e4eeb6188efb0819dcb9c5
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-10-29 15:17:37 +0300

    Chain is now default fallback icon.

--- menus/menu-social.php
+++ menus/menu-social.php
@@ -7,9 +7,9 @@
 ?>
 
 <?php if ( has_nav_menu( 'social' ) ) : // Check if there's a menu assigned to the 'social' location. ?>
-	
+
 	<nav class="menu-social social-navigation menu clear" role="navigation" aria-label="<?php esc_html_e( 'Social Menu', 'checathlon' ); ?>">
-		
+
 		<?php wp_nav_menu(
 			array(
 				'theme_location'  => 'social',
@@ -18,11 +18,11 @@
 				'menu_class'      => 'menu-social-items',
 				'depth'           => 1,
 				'link_before'     => '<span class="screen-reader-text">',
-				'link_after'      => '</span>' . checathlon_get_svg( array( 'icon' => 'rating-full' ) ),
+				'link_after'      => '</span>' . checathlon_get_svg( array( 'icon' => 'chain' ) ),
 				'fallback_cb'     => '',
 			)
 		); ?>
-		
+
 	</nav><!-- .menu-social -->
 
 <?php endif; // End check for menu.
