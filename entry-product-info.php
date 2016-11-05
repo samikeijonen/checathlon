9e0282ee642cfaecf55db9e98112a4ccdeee4b6b
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-11-05 11:03:37 +0200

    Check if purchase link is hidden.

--- entry-product-info.php
+++ entry-product-info.php
@@ -13,7 +13,7 @@ endif;
 	<div class="entry-product-info-wrapper grid-same-line align-items-end">
 		<span class="product-purchase-link">
 			<?php
-				if( edd_has_variable_prices( get_the_ID() ) ) :
+				if ( edd_has_variable_prices( get_the_ID() ) || checathlon_edd_is_purchace_link_hidden() ) :
 					echo '<a class="button" href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . esc_html__( 'Details', 'checathlon' ) . '</a>';
 				else :
 					echo edd_get_purchase_link( apply_filters( 'checathlon_purchace_link',
