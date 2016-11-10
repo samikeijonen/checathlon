cc6a848e8ab5c0dd8c22061c79702bfc3cc4b483
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-11-10 18:05:53 +0200

    Foxland, not Sami Keijonen.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -344,7 +344,7 @@ function checathlon_default_footer_text() { ?>
 
 	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'checathlon' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'checathlon' ), 'WordPress' ); ?></a>
 	<span class="sep"> &middot; </span>
-	<?php printf( esc_html__( 'Theme: %1$s by %2$s', 'checathlon' ), 'Checathlon', '<a href="https://foxland.fi/">Sami Keijonen</a>' );
+	<?php printf( esc_html__( 'Theme: %1$s by %2$s', 'checathlon' ), 'Checathlon', '<a href="https://foxland.fi/">Foxland</a>' );
 }
 
 /**
