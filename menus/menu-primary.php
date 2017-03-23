49a93b631550f07013a544856b06c7d40c232091
Sami Keijonen <sami.keijonen@foxnet.fi>
2017-03-23 14:58:19 +0200

    Use esc_attr_e() in aria-label attribute in menus.

--- menus/menu-primary.php
+++ menus/menu-primary.php
@@ -22,7 +22,7 @@
 	</button>
 
 	<div class="main-navigation-wrapper" id="main-navigation-wrapper">
-		<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_html_e( 'Primary Menu', 'checathlon' ); ?>">
+		<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'checathlon' ); ?>">
 			<?php
 				// Set theme location.
 				$theme_location = checathlon_edd_is_checkout() && has_nav_menu( 'checkout' ) ? 'checkout' : 'primary';
