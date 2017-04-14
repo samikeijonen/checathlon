468a0f7d471a04357dd658bed2b5201ce8bd6157
Sami Keijonen <sami.keijonen@foxnet.fi>
2017-04-14 15:33:18 +0300

    Fix landmark role in footer section: Site title and description is also inside landmark role now.

--- widget-areas/sidebar-footer.php
+++ widget-areas/sidebar-footer.php
@@ -6,16 +6,16 @@
  *
  * @package Checathlon
  */
- 
+
 if ( checathlon_edd_is_checkout() ) :
 	return;
 endif;
 ?>
 
-<div class="footer-widgets-wrapper footer-widgets main-padding">
+<aside class="footer-widgets-wrapper footer-widgets main-padding" role="complementary">
 	<div class="wrapper main-width">
 		<div class="grid-wrapper">
-			
+
 			<div class="site-title-desc-footer-wrapper">
 				<p class="site-title site-title-footer no-margin-bottom title-font"><?php bloginfo( 'name' ); ?></p>
 				<?php
@@ -24,24 +24,24 @@ endif;
 					<p class="site-description site-description-footer no-margin-bottom"><?php echo $description; ?></p>
 				<?php endif; ?>
 			</div><!-- .site-title-desc-footer-wrapper -->
-			
+
 			<?php
 			if( is_active_sidebar( 'sidebar-6' ) ) : ?>
-				<aside id="footer-area-1" class="footer-area-1 footer-widget-area" role="complementary">
+				<div id="footer-area-1" class="footer-area-1 footer-widget-area">
 					<?php dynamic_sidebar( 'sidebar-6' ); ?>
-				</aside><!-- .widget-area -->
+				</div><!-- .widget-area -->
 			<?php endif; ?>
 
 			<?php if ( is_active_sidebar( 'sidebar-7' ) ) : ?>
-				<aside id="footer-area-2" class="footer-area-2 footer-widget-area" role="complementary">
+				<div id="footer-area-2" class="footer-area-2 footer-widget-area">
 					<?php dynamic_sidebar( 'sidebar-7' ); ?>
-				</aside><!-- .widget-area -->
+				</div><!-- .widget-area -->
 			<?php endif; ?>
-		
+
 			<?php
 				get_template_part( 'menus/menu', 'social' ); // Loads the menus/menu-social.php template.
 			?>
-		
+
 		</div><!-- .grid-wrapper -->
 	</div><!-- .wrapper -->
-</div><!-- .footer-widgets-wrapper -->
+</aside><!-- .footer-widgets-wrapper -->
